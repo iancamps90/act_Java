26728c2ed8b80fcaac6f41b709e4aaf3bdd7ca89
iancamps90 <115107407+iancamps90@users.noreply.github.com>
2022-11-03 11:36:57 +0100

    Add files via upload

---
+++ Act3.java
@@ -0,0 +1,26 @@
+package Opeenboot;
+
+public class Act3 {
+
+    /* CREAR UN BUCLE que permita concatenar textos y imprimir el resultado final por consola
+     * concatenar nombres
+     * los textos pueden venir de un array string
+     * se puede usar el metodo concat o si no operador +
+     */
+
+
+    public static void main(String[] args) {
+
+        String[] nombres = {"Ian", "Lidia", "Brian", "Paco", "Lali"};
+
+        for (String nombre : nombres) {
+            System.out.print( nombre + " - ");
+        }
+    }
+
+
+
+
+
+    }
+
